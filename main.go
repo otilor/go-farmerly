3ac89bb85e047225e1dca8ecf51d273f6a313e74
GabielFemi <gabrielfemi799@gmail.com>
2020-05-15 23:29:57 +0200

    add farmerly package

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"github.com/gabielfemi/go-inform/farmerly"
+	"log"
 	"net/http"
 )
 
 func main() {
+	http.Handle("/assets/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 	http.HandleFunc("/", farmerly.Index)
+	log.Println("Server started on: 127.0.0.1:8000")
+	_ = http.ListenAndServe(":8000", nil)
 }
 
