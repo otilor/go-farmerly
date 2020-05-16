948ae0cc2f1bd7ad4440d5ff8bca7b989920a2b6
GabielFemi <gabrielfemi799@gmail.com>
2020-05-16 23:52:38 +0200

    add database utils

---
+++ farmerly/dbConn.go
@@ -0,0 +1,21 @@
+package farmerly
+
+import (
+	"database/sql"
+	_"github.com/go-sql-driver/mysql"
+)
+
+
+func databaseConn() (db *sql.DB) {
+	databaseDriver := "mysql"
+	databaseUser := "root"
+	databasePassword := ""
+	databaseName := "farmerly"
+
+	// mysql, root:Password@/farmerly
+	db, err := sql.Open(databaseDriver, databaseUser +":"+databasePassword+"@/"+databaseName)
+	if err != nil {
+		panic(err.Error())
+ 	}
+ 	return db
+}
