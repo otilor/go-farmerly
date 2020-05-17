6d8dfa508faa75376291d2d51cf7be2654debdf2
GabielFemi <gabrielfemi799@gmail.com>
2020-05-17 17:53:55 +0200

    add parse view function

--- farmerly/dbUtils.go
+++ farmerly/dbUtils.go
@@ -1,1 +1,26 @@
 package farmerly
+type Categories struct {
+	id int
+	name string
+}
+
+func fetchCategories() {
+	db := databaseConn()
+	fetchCategories, err := db.Query("SELECT * FROM categories")
+	isError(err)
+
+
+	categories := Categories{}
+	var result []Categories
+	for fetchCategories.Next() {
+		var id int
+		var name string
+		err = fetchCategories.Scan(&id, &name)
+
+		isError(err)
+
+		categories.id = id
+		categories.name = name
+		result = append(result, categories)
+	}
+}
--- farmerly/view.go
+++ farmerly/view.go
@@ -6,11 +6,18 @@ import (
 	"net/http"
 )
 
-func parseView(w http.ResponseWriter, tmpl string, r *http.Request) {
+func parseEmptyView(w http.ResponseWriter, tmpl string, r *http.Request) {
 	t, err := template.ParseFiles(tmpl)
 	isError(err)
 
 	err = t.Execute(w, nil)
+	isError(err)
+}
+func parseView(w http.ResponseWriter, tmpl string, pageVars []Categories,r *http.Request) {
+	t, err := template.ParseFiles(tmpl)
+	isError(err)
+
+	err = t.Execute(w, pageVars)
 	isError(err)
 }
 
@@ -18,3 +25,4 @@ func parseView(w http.ResponseWriter, tmpl string, r *http.Request) {
 func ViewGen(w http.ResponseWriter, r *http.Request) {
 	parseView(w, "generated_content.gohtml", r)
 }
+
