03b0eeb838683ad05bb2fabcecdbf92f021fb17c
GabielFemi <gabrielfemi799@gmail.com>
2020-05-17 17:18:00 +0200

    add helper for errors

--- farmerly/helpers.go
+++ farmerly/helpers.go
@@ -1,9 +1,14 @@
 package farmerly
 
-
-func hasEmptyValues(values map[string][]string) bool{
-	if len(values) == 0{
+func hasEmptyValues(values map[string][]string) bool {
+	if len(values) == 0 {
 		return true
 	}
 	return false
+}
+
+func isError(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
 }
