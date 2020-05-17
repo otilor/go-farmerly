cb26a7dee98b54633cee15d3121cc1654e38e8e7
GabielFemi <gabrielfemi799@gmail.com>
2020-05-17 17:14:20 +0200

    add connect to database function

--- farmerly/dbUtils.go
+++ farmerly/dbUtils.go
@@ -1,1 +1,19 @@
 package farmerly
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func connectToDatabase() (db *sql.DB) {
+	databaseDriver := "mysql"
+	databaseUser := "root"
+	databasePassword := ""
+	databaseName := "farmerly"
+
+	db, err := sql.Open(databaseDriver, databaseUser+":"+databasePassword+"@/"+databaseName)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
