b95e497af3c1e7dd1c8ef8d846be52695c5d62a5
GabielFemi <gabrielfemi799@gmail.com>
2020-05-17 17:44:35 +0200

    remove redundant code

--- farmerly/dbUtils.go
+++ farmerly/dbUtils.go
@@ -1,17 +1,1 @@
 package farmerly
-
-import (
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-)
-
-func connectToDatabase() (db *sql.DB) {
-	databaseDriver := "mysql"
-	databaseUser := "root"
-	databasePassword := ""
-	databaseName := "farmerly"
-
-	db, err := sql.Open(databaseDriver, databaseUser+":"+databasePassword+"@/"+databaseName)
-	isError(err)
-	return db
-}
