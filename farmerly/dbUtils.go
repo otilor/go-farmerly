4bbc779f1cc0d7a9c1d02ab8b4bf910f867d7f9f
GabielFemi <gabrielfemi799@gmail.com>
2020-05-17 18:04:21 +0200

    fix bug that occurs when you try to fetch just two columns instead of three - id, name, uniqueHash

--- farmerly/dbUtils.go
+++ farmerly/dbUtils.go
@@ -2,6 +2,7 @@ package farmerly
 type Categories struct {
 	id int
 	name string
+	uniqueHash string
 }
 
 func fetchCategories() (res []Categories) {
@@ -14,13 +15,14 @@ func fetchCategories() (res []Categories) {
 	var result []Categories
 	for fetchCategories.Next() {
 		var id int
-		var name string
-		err = fetchCategories.Scan(&id, &name)
+		var name, hash string
+		err = fetchCategories.Scan(&id, &name, &hash)
 
 		isError(err)
 
 		categories.id = id
 		categories.name = name
+		categories.uniqueHash = hash
 		result = append(result, categories)
 	}
 	return result
