94aa6689ff9478c7764add35d5b4a8f7fab5adc6
GabielFemi <gabrielfemi799@gmail.com>
2020-05-19 20:50:06 +0200

    select all posts relating to the current category

--- farmerly/categorize.go
+++ farmerly/categorize.go
@@ -2,3 +2,14 @@
 // It maps farmers to content based on their selection
 
 package farmerly
+
+func sortAccordingToCategory(category string) {
+	db := databaseConn()
+	getPostsAccordingToCategory, err := db.Query("SELECT * FROM posts WHERE category = ?",category)
+	isError(err)
+
+	for  getPostsAccordingToCategory.Next(){
+
+	}
+
+}
