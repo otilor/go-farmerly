f662de31c38e705f5b8873f2d9e196e2db377988
GabielFemi <gabrielfemi799@gmail.com>
2020-05-19 20:58:28 +0200

    finish receiving data from the database and pass it into a struct

--- farmerly/categorize.go
+++ farmerly/categorize.go
@@ -3,14 +3,21 @@
 
 package farmerly
 
-func sortAccordingToCategory(category string) {
+func sortAccordingToCategory(category string) Posts{
 	db := databaseConn()
 	getPostsAccordingToCategory, err := db.Query("SELECT * FROM posts WHERE category = ?",category)
 	isError(err)
 
-
+	posts := Posts{}
 	for  getPostsAccordingToCategory.Next(){
-
+		var id int
+		var title, body, category string
+		err := getPostsAccordingToCategory.Scan(&id, &title, &body, &category)
+		isError(err)
+		posts.Id = id
+		posts.Title = title
+		posts.Body = body
+		posts.Category = category
 	}
-
+	return posts
 }
